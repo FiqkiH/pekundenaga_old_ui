4b73b00bd4bdd3c3ef0821984903606c16afa07a
FiqkiH <89396542+FiqkiH@users.noreply.github.com>
2024-01-03 15:58:31 +0000

    Modified home and footer, delete unnecessary things from previous cloned project

--- app.py
+++ app.py
@@ -2,36 +2,9 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Jakarta, Indonesia',
-    'salary': 'Rp. 1.000.000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Jakarta, Indonesia',
-    'salary': 'Rp. 1.500.000'
-  },
-  {
-    'id': 3,
-    'title': 'Data Engineer',
-    'location': 'Bandung, Indonesia'
-  },
-  {
-    'id': 4,
-    'title': 'Data Munger',
-    'location': 'Tegal, Indonesia',
-    'salary': 'Rp. 1.000.000'
-  }
-]
-
 @app.route("/")
 def hello_world():
-  return render_template("home.html",
-                         jobs = JOBS)
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
